63d1d1ea3bfe9405bc172bea243d27e2ba7b6b9d
Anshul <singhanshul2102@gmail.com>
2025-06-18 21:31:42 +0530

    time change

--- src/pages/JoinUs.tsx
+++ src/pages/JoinUs.tsx
@@ -39,7 +39,7 @@ const [formData, setFormData] = useState<FormDataType>({
   const [timeRemaining, setTimeRemaining] = useState<string>('');
 
   // MODIFIED: Define recruitment period in IST (UTC+5:30)
-  const RECRUITMENT_START = new Date('2025-06-09T00:00:00+05:30'); // Start date in IST
+  const RECRUITMENT_START = new Date('2025-06-18T16:05:00+05:30'); // Start date in IST
   const RECRUITMENT_END = new Date('2025-07-15T23:59:59+05:30');   // End date in IST
 
   // MODIFIED: Helper function to get current time in IST
